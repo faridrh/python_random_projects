7bbda6cddefdd97639e4baaa3808ff572baf0dd2
Farid <farid.rahimoff@gmail.com>
2022-03-28 21:53:10 +0400

    basic controls done

--- snake_tutorial_pygame.py
+++ snake_tutorial_pygame.py
@@ -6,21 +6,51 @@ WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("First Game :)")
 
 WHITE = (255,255,255)
+SPACESHIP_WIDTH = 200
+SPACESHIP_HEIGHT = 200
 
 FPS = 60
+VEL =5
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate (
+    pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (55,40)),90)
 RED_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(
+    pygame.transform.scale(RED_SPACESHIP_IMAGE,(55,40)),270)
 
-
-def draw_window():
-    WIN.fill((WHITE)) # background color
-    WIN.blit(YELLOW_SPACESHIP_IMAGE, (0,0))
+def draw_window(red, yellow):
+    WIN.fill((WHITE)) #background color
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP,(red.x, red.y))
     pygame.display.update() # if not used screen stays black
 
+def yellow_handle_movement(keys_pressed, yellow):
+        if keys_pressed[pygame.K_a]: #LEFT
+            yellow.x -= VEL
+        if keys_pressed[pygame.K_d]: #RIGHT
+            yellow.x += VEL
+        if keys_pressed[pygame.K_w]: #UP
+            yellow.y -= VEL
+        if keys_pressed[pygame.K_s]: #DOWN
+            yellow.y += VEL
+
+def red_handle_movement(keys_pressed, red):
+        if keys_pressed[pygame.K_LEFT]: #LEFT
+            red.x -= VEL
+        if keys_pressed[pygame.K_RIGHT]: #RIGHT
+            red.x += VEL
+        if keys_pressed[pygame.K_UP]: #UP
+            red.y -= VEL
+        if keys_pressed[pygame.K_DOWN]: #DOWN
+            red.y += VEL
+
 def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT )
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -28,8 +58,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        draw_window(red, yellow)
         
     pygame.quit()
 
