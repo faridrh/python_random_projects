e41694688132ceaf320477b391f734d49b462af8
Farid <farid.rahimoff@gmail.com>
2022-03-29 08:44:32 +0400

    adding movement within border

--- snake_tutorial_pygame.py
+++ snake_tutorial_pygame.py
@@ -6,11 +6,13 @@ WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("First Game :)")
 
 WHITE = (255,255,255)
-SPACESHIP_WIDTH = 200
-SPACESHIP_HEIGHT = 200
+BLACK = (0,0,0)
+
+BORDER = pygame.Rect (WIDTH/2 -5, 0, 10, HEIGHT)
 
 FPS = 60
 VEL =5
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT  = 55,40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_yellow.png'))
@@ -23,28 +25,30 @@ RED_SPACESHIP = pygame.transform.rotate(
 
 def draw_window(red, yellow):
     WIN.fill((WHITE)) #background color
+    pygame.draw.rect(WIN, BLACK, BORDER )
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP,(red.x, red.y))
     pygame.display.update() # if not used screen stays black
 
 def yellow_handle_movement(keys_pressed, yellow):
-        if keys_pressed[pygame.K_a]: #LEFT
+        if keys_pressed[pygame.K_a] and yellow.x - VEL> 0: #LEFT
             yellow.x -= VEL
-        if keys_pressed[pygame.K_d]: #RIGHT
+        if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width <BORDER.x: #RIGHT
             yellow.x += VEL
-        if keys_pressed[pygame.K_w]: #UP
+        if keys_pressed[pygame.K_w] and yellow.y - VEL >0 : #UP
             yellow.y -= VEL
-        if keys_pressed[pygame.K_s]: #DOWN
+        if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT -20: #DOWN
             yellow.y += VEL
 
+
 def red_handle_movement(keys_pressed, red):
-        if keys_pressed[pygame.K_LEFT]: #LEFT
+        if keys_pressed[pygame.K_LEFT] and red.x - VEL> BORDER.x +BORDER.width: #LEFT
             red.x -= VEL
-        if keys_pressed[pygame.K_RIGHT]: #RIGHT
+        if keys_pressed[pygame.K_RIGHT] and red.x +VEL +red.width < WIDTH : #RIGHT
             red.x += VEL
-        if keys_pressed[pygame.K_UP]: #UP
+        if keys_pressed[pygame.K_UP] and red.y - VEL >0: #UP
             red.y -= VEL
-        if keys_pressed[pygame.K_DOWN]: #DOWN
+        if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT -20: #DOWN
             red.y += VEL
 
 def main():
